978b003e6121cd8786cecc4ba6cd555b6d2749ec
Buivankiet <86721532+Buivankiet@users.noreply.github.com>
2021-07-01 00:50:55 +1200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
     introTitle: 'Bé à!',
     introDesc: `Làm sao cắt nghĩa được tình yêu,
-    Có nghĩa gì đâu một buổi chiều
+    Có nghĩa gì đâu một buổi chiều.
     Nó chiếm hồn ta bằng nắng nhạt,
     Bằng mây nhè nhẹ, gió hiu hiu `,
     btnIntro: '♥♥♥♥♥',
